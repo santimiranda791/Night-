8dbc9dfb2427d439f5555dd735b24185f7488c1d
nicolas20055 <ortiznicolas656@gmail.com>
2025-04-10 11:53:41 -0500

    Create Header

---
+++ NightPlus/src/assets/Components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+        <h1>Night+</h1>
+        <nav className="navbar">
+            <ul className="nav-list">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
