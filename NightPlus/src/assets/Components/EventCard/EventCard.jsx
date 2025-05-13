83aaf35d4af02e4b18b1e363b0e4bfa4bbc1c0e5
nicolas20055 <ortiznicolas656@gmail.com>
2025-05-13 11:42:27 -0500

    Add card events

---
+++ NightPlus/src/assets/Components/EventCard/EventCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../../../Styles/EventCard.css';
+
+ export const EventCard = () => {
+  return (
+    <div className="event-card">
+      <div className="event-banner">
+        <span className="event-date">VIERNES 09 MAYO</span>
+        <div className="image-container">
+          <img src="/card.png" alt="Evento DJ" className="dj-image" />
+        </div>
+      </div>
+      <div className="event-details">
+        <h2 className="event-title">Noche de Chicas</h2>
+        <p className="event-description">
+          Evento Oficial De Chicas: ¡Combo de 5 Amigas NO SE COBRA COVER!
+        </p>
+        <p className="event-club">Discoteca Tropikal-Club</p>
+        <a href="#" className="event-btn">Ver Evento</a>
+      </div>
+    </div>
+  );
+};
--- NightPlus/src/assets/Components/Pages/Eventos/Eventos.jsx
+++ NightPlus/src/assets/Components/Pages/Eventos/Eventos.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Header } from '../../Header/Header'
+import { EventCard } from '../../EventCard/EventCard'
 export const Eventos = () => {
   return (
    
     <>
     <Header/>
+    <EventCard/>
     </>
   )
 }
